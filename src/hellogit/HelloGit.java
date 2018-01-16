bc86215186d4aee3be0463ef4ac8b97aabc38ab2
Christopher Wijaya <crossraven998@gmail.com>
2018-01-16 17:51:11 +0700

    Show hello world

---
+++ src/hellogit/HelloGit.java
@@ -0,0 +1,17 @@
+
+package hellogit;
+
+/**
+ *
+ * @author Student
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
